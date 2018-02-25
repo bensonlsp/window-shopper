cefaabfd2697ac5f204296b79d5182d79c91c6dd
SHUNPAN LO <shunpanlo@SHUNPANde-MacBook-Air.local>
2018-02-25 15:26:52 +0800

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by SHUNPAN LO on 25/2/2018.
+//  Copyright © 2018年 bensonlspapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
